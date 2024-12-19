54770f1dccd1a86b9a07293296e45b157fc7eff1
TraianDanciu <traian.mihai.danciu@gmail.com>
2024-12-19 10:34:53 +0200

    Added day 19 of Advent of Code 2024

---
+++ advent-of-code/2024/day19/part1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<string, int> mp;
+
+int main() {
+    string s;
+    while(true) {
+        cin >> s;
+        int to_break = 0;
+        if(s.back() == ',') {
+            s.pop_back();
+        } else {
+            to_break = 1;
+        }
+        
+        mp[s] = 1;
+        
+        if(to_break) {
+            break;
+        }
+    }
+    
+    int answer = 0;
+    while(cin >> s) {
+        s = "#" + s;
+        vector<int> dp((int)s.size(), 0);
+        dp[0] = 1;
+        for(int i = 0; i < (int)s.size(); i++) {
+            if(dp[i]) {
+                string t = "";
+                for(int j = i + 1; j < (int)s.size(); j++) {
+                    t.push_back(s[j]);
+                    if(mp[t]) {
+                        dp[j] = 1;
+                    }
+                }
+            }
+        }
+        answer += dp[(int)s.size() - 1];
+    }
+    cout << answer << "\n";
+    return 0;
+}
